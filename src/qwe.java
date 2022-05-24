3d06abeeac9a122b4ed5890dda98d1e8199e495d
Yun531 <jaeyoongh315@gmail.com>
2022-05-24 20:19:32 +0900

    변경

---
+++ src/qwe.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class qwe {
+        public static void main(String[] args){
+            String a = "asd";
+            Scanner as = new Scanner(System.in);
+
+            a = as.next();
+            System.out.println(a);
+
+            return ;
+    }
+}
